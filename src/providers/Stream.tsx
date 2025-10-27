20a5dccae40e7d13c24e1a89e32204424ac0af83
Amr-Abdelaziz <amr.moh.abdelaziz@gmail.com>
2025-10-27 21:03:11 +0300

    ser

--- src/providers/Stream.tsx
+++ src/providers/Stream.tsx
@@ -24,6 +24,7 @@ import { PasswordInput } from "@/components/ui/password-input";
 import { getApiKey } from "@/lib/api-key";
 import { useThreads } from "./Thread";
 import { toast } from "sonner";
+import { SatelliteOrbitIcon } from "@/components/icons/satellite-orbit";
 
 export type StateType = { messages: Message[]; ui?: UIMessage[] };
 
@@ -127,8 +128,8 @@ const StreamSession = ({
 };
 
 // Default values for the form
-const DEFAULT_API_URL = "http://localhost:2024";
-const DEFAULT_ASSISTANT_ID = "agent";
+const DEFAULT_API_URL = "https://agent.dr-ai.tech";
+const DEFAULT_ASSISTANT_ID = "gorbit";
 
 export const StreamProvider: React.FC<{ children: ReactNode }> = ({
   children,
@@ -168,7 +169,7 @@ export const StreamProvider: React.FC<{ children: ReactNode }> = ({
         <div className="animate-in fade-in-0 zoom-in-95 bg-background flex max-w-3xl flex-col rounded-lg border shadow-lg">
           <div className="mt-14 flex flex-col gap-2 border-b p-6">
             <div className="flex flex-col items-start gap-2">
-              <LangGraphLogoSVG className="h-7" />
+              <SatelliteOrbitIcon className="h-7" />
               <h1 className="text-xl font-semibold tracking-tight">
                 Gorbit Chat
               </h1>
